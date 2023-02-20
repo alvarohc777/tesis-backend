9efb9d2709d30ddfca1ad9349006e502af02f2a9
alvarohc777 <aherrada@uninorte.edu.co>
2023-02-20 12:22:21 -0500

    Refactor API endpoints into separate app

---
+++ ninjaserver/fault_detector/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FaultDetectorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fault_detector'
--- ninjaserver/ninjaserver/urls.py
+++ ninjaserver/ninjaserver/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .api import api
+from fault_detector.api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
